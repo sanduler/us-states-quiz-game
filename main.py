f28212d08d5fbba1b0c53f0c077f1ac5e7e44515
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-06 13:49:36 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,7 @@
+# Name: Ruben Sanduleac
+# Date: February 6th, 2021
+# Description: This program is a game that lets the user guess the States in the United States.
+#              The player is able to see the GUI through the turtle library and the data is processed
+#              and anylized through the pandas library.
+import pandas
+from turtle import Turtle
