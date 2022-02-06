e4919df21ac76f0276d679e8f55626124b9fc6a8
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-06 14:03:07 -0800

    added the user input and prompt to guess the State

--- main.py
+++ main.py
@@ -3,10 +3,21 @@
 # Description: This program is a game that lets the user guess the States in the United States.
 #              The player is able to see the GUI through the turtle library and the data is processed
 #              and anylized through the pandas library.
+import turtle
+
 import pandas
 from turtle import Turtle, Screen
 
 screen = Screen()
+# rename the title of the Quiz
 screen.title("U.S. States Quiz")
+# get the directory into a variable
+quiz_image = "./img/blank_states_img.gif"
+# add the shape to the screen
+screen.addshape(quiz_image)
+# print the turtle on the screen
+turtle.shape(quiz_image)
 
-screen.exitonclick()
+
+answer_state = screen.textinput(title="Guess the State", prompt="What is another State's name?")
+turtle.mainloop()
