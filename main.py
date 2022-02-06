32a052384074e4553b8eea658d303c6439271707
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-06 15:18:11 -0800

    finished the basic logic

--- main.py
+++ main.py
@@ -9,15 +9,41 @@ import pandas
 from turtle import Turtle, Screen
 
 screen = Screen()
+pen = Turtle()
+pen.hideturtle()
+score = 0
 # rename the title of the Quiz
-screen.title("U.S. States Quiz")
+screen.title(f"{score}/50 U.S. States Quiz")
 # get the directory into a variable
 quiz_image = "./img/blank_states_img.gif"
 # add the shape to the screen
 screen.addshape(quiz_image)
 # print the turtle on the screen
 turtle.shape(quiz_image)
+US_data = pandas.read_csv("./data/50_states.csv")
+# print(US_data.state[1])
+game_is_playing = True
+guess_list = []
+while game_is_playing:
+    answer_state = screen.textinput(title="Guess the State", prompt="What is another State's name?").title()
+    # print(US_data.x[1])
+    for state in US_data.state:
+        if state == answer_state:
+
+            location = (US_data[US_data.state == str(state)])
+            # print(location.x)
+            location_x = int(location.x)
+            location_y = int(location.y)
+            # print(location_x)
+            guess_list.append(state)
+            pen.penup()
+            pen.goto(location_x, location_y)
+            pen.pendown()
+            pen.write(state, font=("Calibri", 8, "bold"))
+            score += 1
 
 
-answer_state = screen.textinput(title="Guess the State", prompt="What is another State's name?")
+
+
+
 turtle.mainloop()
