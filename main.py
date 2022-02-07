15af7994b6e543f2d178e85f15d3a58d53d7834a
Sanduler <50250694+sanduler@users.noreply.github.com>
2022-02-06 21:55:31 -0800

    added a counter to the quiz

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 # Date: February 6th, 2021
 # Description: This program is a game that lets the user guess the States in the United States.
 #              The player is able to see the GUI through the turtle library and the data is processed
-#              and anylized through the pandas library.
+#              and analyzed through the pandas library.
 import turtle
 
 import pandas
@@ -13,7 +13,7 @@ pen = Turtle()
 pen.hideturtle()
 score = 0
 # rename the title of the Quiz
-screen.title(f"{score}/50 U.S. States Quiz")
+screen.title("U.S. States Quiz")
 # get the directory into a variable
 quiz_image = "./img/blank_states_img.gif"
 # add the shape to the screen
@@ -25,7 +25,7 @@ US_data = pandas.read_csv("./data/50_states.csv")
 all_the_states = US_data.state.to_list()
 guess_list = []
 while len(guess_list) < 50:
-    answer_state = screen.textinput(title="Guess the State", prompt="What is another State's name?").title()
+    answer_state = screen.textinput(title="Guess the State", prompt=f"What is another State's name? {len(guess_list)}/50").title()
     if answer_state in all_the_states:
         pen.penup()
         state_data = US_data[US_data.state == answer_state]
